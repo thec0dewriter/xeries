8205f303604bb9090036a9b8ce44c4228ca17d87
thec0dewriter <kutikmatyas@gmail.com>
2026-04-05 01:21:51 +0300

    Refactor: Comment out unused DartsAdapter import in adapters and add __init__.py for importance module

--- src/xeries/adapters/__init__.py
+++ src/xeries/adapters/__init__.py
@@ -1,13 +1,13 @@
 """Adapters for integrating with forecasting frameworks."""
 
 from xeries.adapters.base import BaseAdapter
-from xeries.adapters.darts import DartsAdapter
+# from xeries.adapters.darts import DartsAdapter
 from xeries.adapters.skforecast import SkforecastAdapter, from_skforecast
 from xeries.adapters.sklearn import SklearnAdapter
 
 __all__ = [
     "BaseAdapter",
-    "DartsAdapter",
+    # "DartsAdapter",
     "SkforecastAdapter",
     "SklearnAdapter",
     "from_skforecast",
